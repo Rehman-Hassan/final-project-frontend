f131ab03793801633fe6e3a29f22a34880101ab0
Your Name <your-email@example.com>
2026-01-22 09:43:12 +0500

    fixed imports

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./Checkout.css";
+import "./CheckOut.css";
 
 const Checkout = () => {
   const { token } = useSelector((state) => state.auth);
